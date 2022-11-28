fe50d4aedf2e1429adb5c7ee8b8813dc1ec3f817
dyu <david.yu.ftw@gmail.com>
2022-11-28 20:50:09 +0800

    stock gcc/clang

--- build.sh
+++ build.sh
@@ -7,12 +7,14 @@ ARCH=`arch`
 TARGET_ARCH='x86_64'
 case "$UNAME" in
     Darwin)
-    [ "$ARCH" = 'arm64' ] && TARGET_ARCH='aarch64'
-    ZCC_OPTS="-target $TARGET_ARCH-macos.11-none" make CC=$PWD/clang -j4
+    #[ "$ARCH" = 'arm64' ] && TARGET_ARCH='aarch64'
+    #ZCC_OPTS="-target $TARGET_ARCH-macos.11-none" make CC=$PWD/clang CXX=$PWD/clang++ -j4
+    make CC=clang CXX=clang++ -j4
     ;;
     *)
-    make CC=$PWD/clang -j4
+    make CC=gcc CXX=g++ -j4
     ;;
 esac
 
 [ "$1" = "install" ] && nimble install -y
+
